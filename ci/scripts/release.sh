8916b3208865b2f2d41d111393df029aec05f66f
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-02 18:22:32 +0100

    initial .onepipeline.yaml and scripts for ci

---
+++ ci/scripts/release.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#
+# prepare data
+#
+
+export GHE_TOKEN="$(cat ../git-token)"
+export COMMIT_SHA="$(cat /config/git-commit)"
+export APP_NAME="$(cat /config/app-name)"
+
+INVENTORY_REPO="$(cat /config/inventory-url)"
+GHE_ORG=${INVENTORY_REPO%/*}
+export GHE_ORG=${GHE_ORG##*/}
+GHE_REPO=${INVENTORY_REPO##*/}
+export GHE_REPO=${GHE_REPO%.git}
+
+export APP_REPO="$(cat /config/repository-url)"
+APP_REPO_ORG=${APP_REPO%/*}
+export APP_REPO_ORG=${APP_REPO_ORG##*/}
+APP_REPO_NAME=${APP_REPO##*/}
+export APP_REPO_NAME=${APP_REPO_NAME%.git}
+
+ARTIFACT="https://raw.github.ibm.com/${APP_REPO_ORG}/${APP_REPO_NAME}/${COMMIT_SHA}/deployment.yml"
+
+IMAGE_ARTIFACT="$(cat /config/artifact)"
+SIGNATURE="$(cat /config/signature)"
+APP_ARTIFACTS='{ "signature": "'${SIGNATURE}'", "provenance": "'${IMAGE_ARTIFACT}'" }'
+#
+# add to inventory
+#
+
+cocoa inventory add \
+    --artifact="${ARTIFACT}" \
+    --repository-url="${APP_REPO}" \
+    --commit-sha="${COMMIT_SHA}" \
+    --build-number="${BUILD_NUMBER}" \
+    --pipeline-run-id="${PIPELINE_RUN_ID}" \
+    --version="$(cat /config/version)" \
+    --name="${APP_NAME}_deployment"
+
+cocoa inventory add \
+    --artifact="${IMAGE_ARTIFACT}" \
+    --repository-url="${APP_REPO}" \
+    --commit-sha="${COMMIT_SHA}" \
+    --build-number="${BUILD_NUMBER}" \
+    --pipeline-run-id="${PIPELINE_RUN_ID}" \
+    --version="$(cat /config/version)" \
+    --name="${APP_NAME}" \
+    --app-artifacts="${APP_ARTIFACTS}"
