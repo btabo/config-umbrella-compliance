1637bbe7762549264a4924e147512ab0bc05f651
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-05 18:18:17 +0100

    debug

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -8,3 +8,13 @@ docker push "$IMAGE"
 echo -n $(docker inspect --format='{{index .RepoDigests 0}}' "$IMAGE" | awk -F@ '{print $2}') > ../../image-digest
 echo -n "$IMAGE_TAG" > ../../image-tags
 echo -n "$IMAGE" > ../../image
+
+echo cat ../../image-digest
+cat ../../image-digest
+echo
+echo cat ../../image-tags
+cat ../../image-tags
+echo
+echo cat ../../image
+cat ../../image
+echo
