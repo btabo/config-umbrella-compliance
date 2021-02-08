6a5609934b6064fe5a4073b9367809e358093707
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-08 16:22:23 +0100

    deploy to test cluster

--- ci/common/deploy_setup.sh
+++ ci/common/deploy_setup.sh
@@ -1,38 +1,11 @@
 #!/usr/bin/env bash
 CONFIG_FOLDER=${1:-"/config"}
 
-export IBMCLOUD_API_KEY
-export IBMCLOUD_TOOLCHAIN_ID
-export IBMCLOUD_IKS_REGION
-export IBMCLOUD_IKS_CLUSTER_NAME
-export IBMCLOUD_IKS_CLUSTER_NAMESPACE
-export REGISTRY_URL
-export IMAGE_PULL_SECRET_NAME
-export IMAGE
-export HOME
-export BREAK_GLASS
-
-if [ -f $CONFIG_FOLDER/api-key ]; then
-  IBMCLOUD_API_KEY="$(cat $CONFIG_FOLDER/api-key)" # pragma: allowlist secret
-else
-  IBMCLOUD_API_KEY="$(cat $CONFIG_FOLDER/ibmcloud-api-key)" # pragma: allowlist secret
-fi
-
-HOME=/root
-IBMCLOUD_TOOLCHAIN_ID="$(jq -r .toolchain_guid /toolchain/toolchain.json)"
-IBMCLOUD_IKS_REGION="$(cat $CONFIG_FOLDER/dev-region | awk -F ":" '{print $NF}')"
-IBMCLOUD_IKS_CLUSTER_NAMESPACE="$(cat $CONFIG_FOLDER/dev-cluster-namespace)"
-IBMCLOUD_IKS_CLUSTER_NAME="$(cat $CONFIG_FOLDER/cluster-name)"
-REGISTRY_URL="$(cat $CONFIG_FOLDER/image | awk -F/ '{print $1}')"
-IMAGE="$(cat $CONFIG_FOLDER/image)"
-IMAGE_PULL_SECRET_NAME="ibmcloud-toolchain-${IBMCLOUD_TOOLCHAIN_ID}-${REGISTRY_URL}"
-BREAK_GLASS=$(cat $CONFIG_FOLDER/break_glass || echo false)
-
-if [[ "$BREAK_GLASS" == true ]]; then
-  export KUBECONFIG
-  KUBECONFIG=$CONFIG_FOLDER/cluster-cert
-else
-  IBMCLOUD_IKS_REGION=$(echo "${IBMCLOUD_IKS_REGION}" | awk -F ":" '{print $NF}')
-  ibmcloud login -r "$IBMCLOUD_IKS_REGION"
-  eval "$(ibmcloud ks cluster config --cluster "$IBMCLOUD_IKS_CLUSTER_NAME" --export)"
-fi
+if [ ! -d "otc-deploy" ]; then
+  git clone "https://$GH_TOKEN@github.ibm.com/org-ids/otc-deploy"
+fi 
+if [ ! -d "devops-config" ]; then
+  git clone "https://$GH_TOKEN@github.ibm.com/ids-env/devops-config"
+fi 
+export IC_1308775_API_KEY=$(cat $CONFIG_FOLDER/IC_1308775_API_KEY)
+. otc-deploy/k8s/scripts/login/clusterLogin.sh "otc-dal12-test"
