def7835fde51f40b3076d5b5e35bb7cb6f92cb01
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-10 11:16:47 +0100

    more env vars for deploy

--- ci/otc-pagerduty-broker/deploy_config.sh
+++ ci/otc-pagerduty-broker/deploy_config.sh
@@ -12,3 +12,11 @@ export DOMAIN="otc-dal12-test.us-south.containers.mybluemix.net"
 export GLOBAL_ENV_SECGRP="GRP3DEVS"
 export ENV_url="https://$COMPONENT_NAME.$DOMAIN"
 export PIPELINE_KUBERNETES_CLUSTER_NAME="otc-dal12-test"
+
+export CLOUDANT_URL=$(cat $CONFIG_FOLDER/pagerduty_CLOUDANT_URL)
+export LOG4J_LEVEL='DEBUG'
+export PORT='8080'
+export TIAM_URL='https://tiam.us-south.devops.dev.cloud.ibm.com/identity/v1'
+export services__otc_api='https://otc-api.us-south.devops.dev.cloud.ibm.com/api/v1'
+export services__otc_ui='https://otc-ui.us-south.devops.dev.cloud.ibm.com/devops'
+export services__otc_ui_env_id='ibm:ys1:us-south'
