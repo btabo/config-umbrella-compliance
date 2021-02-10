bec771e192b7319e3c5449ad85cf88eb7bc1f18e
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-10 13:52:08 +0100

    config and secrets for pagerduty tests

--- ci/otc-pagerduty-broker/deploy_config.sh
+++ ci/otc-pagerduty-broker/deploy_config.sh
@@ -2,6 +2,25 @@
 
 CONFIG_FOLDER=$1
 
+# secrets
+export SEC_OTC_API_BROKER_SECRET=$(cat $CONFIG_FOLDER/pagerduty_OTC_API_SECRET)       
+export pagerduty_api_key=$(cat $CONFIG_FOLDER/pagerduty_api_key)
+export pagerduty_api_key_2=$(cat $CONFIG_FOLDER/pagerduty_api_key_2)
+export test_tiam_secret=$(cat $CONFIG_FOLDER/pagerduty_test_tiam_secret)
+export SEC_ENCRYPTION_KEY=$(cat $CONFIG_FOLDER/pagerduty_ENCRYPTION_KEY)
+export SEC_CLOUDANT_IAM_API_KEY=$(cat $CONFIG_FOLDER/pagerduty_CLOUDANT_IAM_API_KEY)
+ 
+# config 
+export ENV_CLOUDANT_URL=$(cat $CONFIG_FOLDER/pagerduty_CLOUDANT_URL)
+export ENV_LOG4J_LEVEL="DEBUG"
+export ENV_services__otc_api="https://otc-api.us-south.devops.dev.cloud.ibm.com/api/v1"
+export ENV_services__otc_ui="https://otc-ui.us-south.devops.dev.cloud.ibm.com/devops"
+export ENV_services__otc_ui_env_id='ibm:ys1:us-south'
+export ENV_TIAM_URL="https://tiam.us-south.devops.dev.cloud.ibm.com/identity/v1"
+export pagerduty_site_name="ibmdevops"
+export pagerduty_site_name_2="ibmdevops"
+export test_tiam_id="test"
+export ENV_PORT="8080"
 export NAMESPACE="opentoolchain"
 export NUM_INSTANCES='1'
 export RELEASE_NAME=$(cat $CONFIG_FOLDER/app-name)
@@ -12,11 +31,3 @@ export DOMAIN="otc-dal12-test.us-south.containers.mybluemix.net"
 export GLOBAL_ENV_SECGRP="GRP3DEVS"
 export ENV_url="https://$COMPONENT_NAME.$DOMAIN"
 export PIPELINE_KUBERNETES_CLUSTER_NAME="otc-dal12-test"
-
-export CLOUDANT_URL=$(cat $CONFIG_FOLDER/pagerduty_CLOUDANT_URL)
-export LOG4J_LEVEL='DEBUG'
-export PORT='8080'
-export TIAM_URL='https://tiam.us-south.devops.dev.cloud.ibm.com/identity/v1'
-export services__otc_api='https://otc-api.us-south.devops.dev.cloud.ibm.com/api/v1'
-export services__otc_ui='https://otc-ui.us-south.devops.dev.cloud.ibm.com/devops'
-export services__otc_ui_env_id='ibm:ys1:us-south'
