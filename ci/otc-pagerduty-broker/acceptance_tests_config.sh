c2eb1e18733aa3d056cadfbb3e1fbaacdd33ecf3
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-12 17:29:54 +0100

    add missing env vars for tests

--- ci/otc-pagerduty-broker/acceptance_tests_config.sh
+++ ci/otc-pagerduty-broker/acceptance_tests_config.sh
@@ -3,3 +3,7 @@ OTC_PAGERDUTY_BROKER_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # same config as for test cluster deployment
 . $OTC_PAGERDUTY_BROKER_FOLDER/deploy_config.sh
+
+export TIAM_URL=$ENV_TIAM_URL
+export _DEPLOY_url=$ENV_url
+export _DEPLOY_OTC_API_BROKER_SECRET=$SEC_OTC_API_BROKER_SECRET
