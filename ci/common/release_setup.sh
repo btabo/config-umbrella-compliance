5eb3c81f8a5ed87da1276ebe9b5c3357f425443c
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-15 16:23:24 +0100

    install cocoa cli

--- ci/common/release_setup.sh
+++ ci/common/release_setup.sh
@@ -37,7 +37,16 @@ export CHART_ORG="ids-env"
 #export TRIGGER_BRANCH="trigger-deploy"
 export LOGICAL_APP_NAME="$APP_NAME"
 export BUILD_PREFIX="$BRANCH"
+export ARTIFACTORY_ID=idsorg@us.ibm.com
+export ARTIFACTORY_TOKEN_BASE64="$(cat $CONFIG_FOLDER/ARTIFACTORY_TOKEN_BASE64)"
 
 export IC_1308775_API_KEY=$(cat $CONFIG_FOLDER/IC_1308775_API_KEY)
 . otc-deploy/k8s/scripts/login/clusterLogin.sh "otc-dal12-test"
 . otc-deploy/k8s/scripts/helpers/checkHelmVersion.sh
+
+# install cocoa cli
+COCOA_CLI_VERSION=1.4.0
+ARTIFACTORY_API_KEY=$(base64 -D <<< $ARTIFACTORY_TOKEN_BASE64)
+curl -u ${ARTIFACTORY_ID}:${ARTIFACTORY_API_KEY} -O "https://eu.artifactory.swg-devops.com/artifactory/wcp-compliance-automation-team-generic-local/cocoa-linux-${COCOA_CLI_VERSION}"
+cp cocoa-linux-* /usr/local/bin/cocoa
+chmod +x /usr/local/bin/cocoa
