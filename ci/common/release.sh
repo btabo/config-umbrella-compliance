87d783351bd567aee3989d1b72442e73587903c6
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-02-16 11:38:04 +0100

    formatting

--- ci/common/release.sh
+++ ci/common/release.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 CONFIG_FOLDER=${1:-"/config"}
 
+# build and publish component chart to solution repo
 chmod u+x otc-deploy/k8s/scripts/ci/publishHelmChart.sh
 ./otc-deploy/k8s/scripts/ci/publishHelmChart.sh
 
+# add to inventory
 CHART_VERSION=$(yq r -j "k8s/$APP_NAME/Chart.yaml" | jq -r '.version')
 ARTIFACT="https://github.ibm.com/$CHART_ORG/$CHART_REPO/blob/master/charts/$APP_NAME-$CHART_VERSION.tgz"
-
 IMAGE_ARTIFACT="$(cat $CONFIG_FOLDER/artifact)"
 if [ -f $CONFIG_FOLDER/signature ]; then
     # using TaaS worker
@@ -16,14 +17,6 @@ else
     # using regular worker, no signature
     APP_ARTIFACTS='{ "provenance": "'${IMAGE_ARTIFACT}'" }'
 fi
-
-echo which cocoa
-which cocoa
-echo
-
-#
-# add to inventory
-#
 cocoa inventory add \
     --artifact="${ARTIFACT}" \
     --repository-url="${APP_REPO}" \
@@ -32,7 +25,6 @@ cocoa inventory add \
     --pipeline-run-id="${PIPELINE_RUN_ID}" \
     --version="$(cat $CONFIG_FOLDER/version)" \
     --name="${APP_NAME}"
-
 cocoa inventory add \
     --artifact="${IMAGE_ARTIFACT}" \
     --repository-url="${APP_REPO}" \
