66c255e6ab37f8e52b53b81f6cc9daf7d91327e5
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-03-08 18:28:57 +0100

    unset vars not used by cti nock tests

--- ci/otc-cti-broker/test_config.sh
+++ ci/otc-cti-broker/test_config.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
 # secrets
-export OTC_API_BROKER_SECRET=$(get_env otc-cti-broker_OTC_API_SECRET)       
+export OTC_API_BROKER_SECRET=$(get_env otc-cti-broker_OTC_API_SECRET)
+unset test_tiam_secret
 
 # config
-# none
+unset CLOUDANT_URL
+unset TIAM_URL
+unset services__otc_api
+unset services__otc_ui
+unset test_tiam_id
 
 # script file
 export TESTS_SCRIPT_FILE=".jobs/nock.sh"
