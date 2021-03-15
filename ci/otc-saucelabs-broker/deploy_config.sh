40b7b93f40bfefabeb7cac267b7d15662ef02d26
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-03-15 11:28:26 +0100

    fix typos

--- ci/otc-saucelabs-broker/deploy_config.sh
+++ ci/otc-saucelabs-broker/deploy_config.sh
@@ -4,8 +4,8 @@
 export SEC_OTC_API_BROKER_SECRET=$(get_env otc-saucelabs-broker_OTC_API_SECRET)
 
 # config 
-export ENV_SAUCELABS_USERNAME: erics_ibm
-export ENV_ORGANIZATION_GUID: 83e0652c-20b3-498e-a19c-402f81625c24
-export ENV_TEST_USER: pipeauto@us.ibm.com
-export ENV_TEST_REPORT_USER: theozparksatspooninskull
-export ENV_TEST_REPORT_URL: https://ci-tru.mybluemix.net/php/utilities/uploadTestResults.php
+export ENV_SAUCELABS_USERNAME="erics_ibm"
+export ENV_ORGANIZATION_GUID="83e0652c-20b3-498e-a19c-402f81625c24"
+export ENV_TEST_USER="pipeauto@us.ibm.com"
+export ENV_TEST_REPORT_USER="theozparksatspooninskull"
+export ENV_TEST_REPORT_URL="https://ci-tru.mybluemix.net/php/utilities/uploadTestResults.php"
