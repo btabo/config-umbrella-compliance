1710312f146815796ddc6f386f8ebd36799a18c5
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-03-15 10:52:10 +0100

    saucelabs broker

---
+++ ci/otc-saucelabs-broker/acceptance_test_config.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# secrets
+#export TEST_PASSWORD=$(get_env otc-saucelabs-broker_TEST_PASSWORD)
+#export TEST_REPORT_PASS=$(get_env otc-saucelabs-broker_TEST_REPORT_PASS)
+#export SAUCELABS_KEY=$(get_env otc-saucelabs-broker_SAUCELABS_KEY)
+
+# config
+# none
+
+# script file
+export ACCEPTANCE_TESTS_SCRIPT_FILE="deploy/test_api.sh"
