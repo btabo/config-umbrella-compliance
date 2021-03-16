94227a0224e7e18877887cc7c91733e86fc7084c
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-03-16 13:55:56 +0100

    more ENV_ var to pass helm lint

--- ci/otc-toolint-broker/release_config.sh
+++ ci/otc-toolint-broker/release_config.sh
@@ -2,3 +2,5 @@
 
 # to pass helm lint --strict
 export ENV_CF_API_URL="topasshelmlintanddryrun"
+export ENV_TOOLCHAIN_BASE_URL="topasshelmlintstrict"
+export ENV_WEBHOOK_PUBLISH_URL="topasshelmlintstrict"
