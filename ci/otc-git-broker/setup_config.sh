ac2e028f771efb45fa04a966a7850817dcc1afa9
jaunin-b <jaunin.b@fr.ibm.com>
2021-03-22 10:13:04 +0100

    Add setup_config for build

---
+++ ci/otc-git-broker/setup_config.sh
@@ -0,0 +1,31 @@
+export HOME=/root
+. ~/.nvm/nvm.sh
+npm --version
+if [[ ! -f ~/.netrc ]]; then
+    cat > ~/.netrc <<NETRC
+machine github.ibm.com
+login idsorg
+password $IDSORG_TOKEN
+NETRC
+    chmod 600 ~/.netrc
+fi
+if [ "$INSTALL_BUILD_ESSENTIAL" ]; then
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential
+fi
+cd $WORKSPACE
+echo -e "@otc-core:registry=https://na.artifactory.swg-devops.com/artifactory/api/npm/wcp-otc-core-team-npm-local/ \n_password=${ARTIFACTORY_TOKEN_BASE64} \nalways-auth=true \nemail=${ARTIFACTORY_ID} \nusername=${ARTIFACTORY_ID}\n@console:registry=https://na.artifactory.swg-devops.com/artifactory/api/npm/wcp-tmp-ace-fr-team-npm-virtual/ \n_password=${ARTIFACTORY_TOKEN_BASE64} \nalways-auth=true \nemail=${ARTIFACTORY_ID} \nusername=${ARTIFACTORY_ID}" > .npmrc
+GIT_COMMIT=$(git log --format="%H" -n 1)
+if [ "$RUN_WEBPACK_BUILD" ]; then
+npm install
+export NODE_ENV=production
+npm run build && npm prune --production
+else
+echo "Running npm install --production"
+npm install --production
+fi
+
+echo "$(get_env branch)-$(date +%Y%m%d%H%M%Z)-${GIT_COMMIT}" > $WORKSPACE/.pipeline_build_id
+echo "${GIT_COMMIT}-$(date +%Y%m%d%H%M%Z)" > $WORKSPACE/.k8s_build_id
+echo -e "{\"build\":\""$(date +%Y%m%d%H%M%S%Z)"\", \"appName\":\""${APP_NAME}"\", \"platform\":\""Armada"\", \"commit\":\""$GIT_COMMIT"\"}" > $WORKSPACE/build.json
+echo "Build id: $(cat $WORKSPACE/.pipeline_build_id)"
