bfcb65b59214879b3ff3d6723390aa8c9269f23c
jaunin-b <jaunin.b@fr.ibm.com>
2021-03-30 16:10:09 +0200

    Add deploy_config info

--- ci/otc-bm-helper/deploy_config.sh
+++ ci/otc-bm-helper/deploy_config.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # secrets
- 
+export SEC_SESSION_SECRET=$(get_env otc-bm-helper_SESSION_SECRET)
+
 # config 
+export ENV_VCAP_APPLICATION='{"application_uris":["devops-dal12-test.us-south.containers.mybluemix.net"]}'
 
 # none
