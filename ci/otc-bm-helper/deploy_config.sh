f70a4251916b7370bc62d64237f0360bc69b21c0
jaunin-b <jaunin.b@fr.ibm.com>
2021-03-30 17:06:14 +0200

    Set use_vault: true as EXTRA

--- ci/otc-bm-helper/deploy_config.sh
+++ ci/otc-bm-helper/deploy_config.sh
@@ -7,3 +7,5 @@ export SEC_SESSION_SECRET=$(get_env otc-bm-helper_SESSION_SECRET)
 export ENV_VCAP_APPLICATION='{"application_uris":["devops-dal12-test.us-south.containers.mybluemix.net"]}'
 
 # none
+# To ensure the deployment to test is successful in order to use vault entry
+export EXTRA="use_vault: true"
