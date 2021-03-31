8651a031c2a52e5e2853f628952654e1d17e7541
jaunin-b <jaunin.b@fr.ibm.com>
2021-03-31 17:27:05 +0200

    Fix path for setup_config

--- ci/git-cas/setup_config.sh
+++ ci/git-cas/setup_config.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Use the same setup_script content as otc-git-broker
-export SETUP_SCRIPT_FILE="$WORKSPACE/app/one-pipeline-config-repo/ci/otc-git-broker/setup_config.sh"
+export SETUP_SCRIPT_FILE="$WORKSPACE/one-pipeline-config-repo/ci/otc-git-broker/setup_config.sh"
