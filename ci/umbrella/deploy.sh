7c1a21279f7dd8573057749f64b759ae1c6d0106
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-04-27 14:34:23 +0200

    install otc-deploy scripts

--- ci/umbrella/deploy.sh
+++ ci/umbrella/deploy.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# since we're not using otc-deploy image any longer, need to install scripts manually
+UMBRELLA_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $UMBRELLA_FOLDER/../common/helpers.sh
+cloneOtcDeploy
+cp -r otc-deploy/k8s/scripts/* /
+cp -r devops-config/otc-deploy/* /otc-config
+
 source ./cd/deploy.sh
