a0b1625ceb0d56fd861ee8bc3419a16a82e287fd
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-04-27 14:45:58 +0200

    install scripts before acceptance tests

--- ci/umbrella/acceptance_test.sh
+++ ci/umbrella/acceptance_test.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
+
+# since we're not using otc-deploy image any longer, need to install scripts manually
+# UMBRELLA_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# source $UMBRELLA_FOLDER/../common/helpers.sh
+# cloneOtcDeploy
+cp -r otc-deploy/k8s/scripts/* /
+cp -r devops-config/otc-deploy/* /otc-config
+
+echo ls -la /
+ls -la /
+echo
+
 source ./cd/acceptance_test.sh
