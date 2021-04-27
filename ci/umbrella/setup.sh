273c1d99c86f1f2ff94f95df3a147d21b91f501d
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-04-27 14:24:48 +0200

    fix helpers.sh folder

--- ci/umbrella/setup.sh
+++ ci/umbrella/setup.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # since we're not using otc-deploy image any longer, need to install scripts manually
-source ./ci/helpers.sh
+UMBRELLA_FOLDER="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $UMBRELLA_FOLDER/../common/helpers.sh
 cloneOtcDeploy
 cp -r otc-deploy/k8s/scripts/* /
 cp -r devops-config/otc-deploy/* /otc-config
