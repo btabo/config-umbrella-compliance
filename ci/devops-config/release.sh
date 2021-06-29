dc34bb9759296a42e321a2ec5fd74e24c9c039e5
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-06-29 09:28:14 +0200

    upload config to artifactory
    https://github.ibm.com/org-ids/roadmap/issues/17835

--- ci/devops-config/release.sh
+++ ci/devops-config/release.sh
@@ -13,6 +13,7 @@ export COMMIT_SHA="$(get_env git-commit)"
 
 # install scripts helpers
 cloneOtcDeploy
+source otc-deploy/k8s/scripts/artifactory/helpers.sh
 source otc-deploy/k8s/scripts/umbrella/helpers.sh
 
 # for cocoa cli
@@ -39,38 +40,45 @@ export ARTIFACTORY_ID=idsorg@us.ibm.com
 export ARTIFACTORY_API_KEY="$(get_env otc_ARTIFACTORY_API_KEY)"
 installCocoa	
 
+# prepare tgz file
+VERSION=$(date -u +%Y%m%d%H%M%Z)
+TGZ_FILE_NAME="config-$VERSION.tgz"
+tar -zcf $TGZ_FILE_NAME ./devops-config/environments
+
 echo "Adding to inventory"
 if [ "$HAS_DEV_CHANGES" == "true" ]; then
+    uploadToArtifactory ARTIFACTORY_SHA_256 ARTIFACTORY_DOWNLOAD_URI "$TGZ_FILE_NAME" "wcp-otc-common-team-generic-local" "helm-charts/dev/$TGZ_FILE_NAME" $ARTIFACTORY_API_KEY    
     cocoa inventory add \
         --org="$INVENTORY_ORG" \
         --repo="$INVENTORY_REPO" \
         --environment="dev" \
         --name="config" \
-        --artifact="environments-config-values" \
+        --artifact="$TGZ_FILE_NAME" \
         --repository-url="${APP_REPO}" \
         --commit-sha="${COMMIT_SHA}" \
         --build-number="${BUILD_NUMBER}" \
         --pipeline-run-id="${PIPELINE_RUN_ID}" \
-        --version="$(date -u +%Y%m%d%H%M%Z)" \
+        --version="${VERSION}" \
         --type="config" \
-        --sha256="${COMMIT_SHA}" \
-        --provenance="https://github.ibm.com/ids-env/devops-config/tree/${COMMIT_SHA}/environments"
+        --sha256="${ARTIFACTORY_SHA_256}" \
+        --provenance="${ARTIFACTORY_DOWNLOAD_URI}"
 fi
 if [ "$HAS_STAGING_CHANGES" == "true" ]; then
+    uploadToArtifactory ARTIFACTORY_SHA_256 ARTIFACTORY_DOWNLOAD_URI "$TGZ_FILE_NAME" "wcp-otc-common-team-generic-local" "helm-charts/prod/$TGZ_FILE_NAME" $ARTIFACTORY_API_KEY    
     cocoa inventory add \
         --org="$INVENTORY_ORG" \
         --repo="$INVENTORY_REPO" \
         --environment="staging" \
         --name="config" \
-        --artifact="environments-config-values" \
+        --artifact="$TGZ_FILE_NAME" \
         --repository-url="${APP_REPO}" \
         --commit-sha="${COMMIT_SHA}" \
         --build-number="${BUILD_NUMBER}" \
         --pipeline-run-id="${PIPELINE_RUN_ID}" \
-        --version="$(date -u +%Y%m%d%H%M%Z)" \
+        --version="${VERSION}" \
         --type="config" \
-        --sha256="${COMMIT_SHA}" \
-        --provenance="https://github.ibm.com/ids-env/devops-config/tree/${COMMIT_SHA}/environments"
+        --sha256="${ARTIFACTORY_SHA_256}" \
+        --provenance="${ARTIFACTORY_DOWNLOAD_URI}"
 fi
 echo "Inventory updated"
 echo
