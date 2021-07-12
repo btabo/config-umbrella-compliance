e2392f6ec1ce30ab909ece17c73241274e23173c
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2021-07-12 11:16:58 +0200

    health check not available in integration
    https://otc-github-consolidated-broker-otc-int.otc-dal12-test.us-south.containers.mybluemix.net/status returns 501

--- ci/otc-git-broker/acceptance_test_config.sh
+++ ci/otc-git-broker/acceptance_test_config.sh
@@ -4,6 +4,9 @@
 
 # config
 export TEST_URL="https://otc-github-consolidated-broker-$NAMESPACE.$DOMAIN/status"
+if [ "$NAMESPACE" == "otc-int" ]; then
+    export SKIP_HEALTH_CHECK="true"
+fi
 
 # script file
 export ACCEPTANCE_TESTS_SCRIPT_FILE=""
