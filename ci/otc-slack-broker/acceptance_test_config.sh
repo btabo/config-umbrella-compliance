290ec9cc6554f98c0ad4fb3604dd45a57d78913a
Navneet Kaushal <navneet.kaushal@ibm.com>
2021-09-29 15:58:40 -0400

    Add ENCRYPTION_KEY

--- ci/otc-slack-broker/acceptance_test_config.sh
+++ ci/otc-slack-broker/acceptance_test_config.sh
@@ -3,6 +3,7 @@
 # secrets
 export _DEPLOY_OTC_API_BROKER_SECRET=$(get_env otc-slack-broker_OTC_API_SECRET)
 export slack_token=$(get_env otc-slack-broker_slack_token)
+export ENCRYPTION_KEY=$(get_env otc-slack-broker_ENCRYPTION_KEY)
 
 #config
 export slack_domain=https://idstest.slack.com
