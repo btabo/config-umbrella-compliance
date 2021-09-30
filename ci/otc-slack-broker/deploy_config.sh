d627fe37e0fdfcd498316bb0f9e8d32ef7fbe329
Navneet Kaushal <Navneet.Kaushal@ibm.com>
2021-09-30 11:43:10 -0400

    Add encryption key to be available in the test pod

--- ci/otc-slack-broker/deploy_config.sh
+++ ci/otc-slack-broker/deploy_config.sh
@@ -2,6 +2,7 @@
 
 # secrets
 export SEC_OTC_API_BROKER_SECRET=$(get_env otc-slack-broker_OTC_API_SECRET)
- 
+export SEC_ENCRYPTION_KEY=$(get_env otc-slack-broker_ENCRYPTION_KEY)
+
 # config 
 # none
