510f4f62dea3a47bc75ea29c9e008683181137fd
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2022-01-07 17:07:29 +0100

    removing git broker since no longer part of umbrella

--- ci/otc-git-broker/release_config.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# to pass helm lint --strict
-export GLOBAL_ENV_SECGRP="topasshelmlintanddryrun"
-export GLOBAL_SEC__vcap_services__cloudantNoSQLDB__0__credentials__url="topasshelmlintanddryrun"
-export ENV_url="topasshelmlintanddryrun"
-export ENV_domain="topasshelmlintanddryrun"
-export ENV_services__env_id="topasshelmlintanddryrun"
