0571a318937ef0c9bbb7db4c1e81d716bba595d5
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2022-01-10 15:11:12 +0100

    copy setup.sh from old otc-github-broker

---
+++ ci/git-cas/setup.sh
@@ -0,0 +1,37 @@
+# Copy of otc-git-broker/setup_config.sh after it was removed
+
+INSTALL_BUILD_ESSENTIAL="true"
+RUN_WEBPACK_BUILD="true"
+############
+export HOME=/root
+. ~/.nvm/nvm.sh
+npm --version
+ls -l ~
+if [[ ! -f ~/.netrc ]]; then
+    cat > ~/.netrc <<NETRC
+machine github.ibm.com
+login idsorg
+password $IDS_TOKEN
+NETRC
+    chmod 600 ~/.netrc
+fi
+if [ "$INSTALL_BUILD_ESSENTIAL" ]; then
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y install build-essential
+fi
+
+echo -e "@otc-core:registry=https://na.artifactory.swg-devops.com/artifactory/api/npm/wcp-otc-core-team-npm-local/ \n_password=${ARTIFACTORY_TOKEN_BASE64} \nalways-auth=true \nemail=${ARTIFACTORY_ID} \nusername=${ARTIFACTORY_ID}\n@console:registry=https://na.artifactory.swg-devops.com/artifactory/api/npm/wcp-tmp-ace-fr-team-npm-virtual/ \n_password=${ARTIFACTORY_TOKEN_BASE64} \nalways-auth=true \nemail=${ARTIFACTORY_ID} \nusername=${ARTIFACTORY_ID} \nregistry=https://na.artifactory.swg-devops.com/artifactory/api/npm/wcp-otc-common-team-npm-virtual/ \n_auth=${ARTIFACTORY_AUTH_BASE64} \nalways-auth=true \nemail=${ARTIFACTORY_ID}" > .npmrc
+GIT_COMMIT=$(git log --format="%H" -n 1)
+if [ "$RUN_WEBPACK_BUILD" ]; then
+npm install
+export NODE_ENV=production
+npm run build && npm prune --production
+else
+echo "Running npm install --production"
+npm install --production
+fi
+
+echo "$(get_env branch)-$(date +%Y%m%d%H%M%Z)-${GIT_COMMIT}" > .pipeline_build_id
+echo "${GIT_COMMIT}-$(date +%Y%m%d%H%M%Z)" > .k8s_build_id
+echo -e "{\"build\":\""$(date +%Y%m%d%H%M%S%Z)"\", \"appName\":\""${APP_NAME}"\", \"platform\":\""Armada"\", \"commit\":\""$GIT_COMMIT"\"}" > build.json
+echo "Build id: $(cat .pipeline_build_id)"
