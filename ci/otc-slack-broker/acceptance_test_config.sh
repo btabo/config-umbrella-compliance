e81be9153b463302a3af8248729ce1acac42dd88
Olivier Thomann <Olivier_Thomann@ca.ibm.com>
2022-02-14 17:36:13 -0500

    Update slack tests settings

--- ci/otc-slack-broker/acceptance_test_config.sh
+++ ci/otc-slack-broker/acceptance_test_config.sh
@@ -2,8 +2,12 @@
 
 # secrets
 export _DEPLOY_OTC_API_BROKER_SECRET=$(get_env otc-slack-broker_OTC_API_SECRET)
-export slack_token=$(get_env otc-slack-broker_slack_token)
 export ENCRYPTION_KEY=$(get_env otc-slack-broker_ENCRYPTION_KEY)
+export slack_webhook=$(get_env otc-slack-broker_slack_webhook)
+export archived_slack_webhook=$(get_env otc-slack-broker_archived_slack_webhook)
+export slack_user_token=$(get_env otc-slack-broker_slack_user_token)
+export channel_id_for_tests=$(get_env otc-slack-broker_channel_id)
+export channel_name_for_tests=$(get_env otc-slack-broker_channel_name)
 
 #config
 export slack_domain=https://idstest.slack.com
