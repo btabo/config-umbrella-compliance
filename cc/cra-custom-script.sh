f382df63adfdb9c123a2d6a228474c4c367da1ca
Jerome Lanneluc <jerome_lanneluc@fr.ibm.com>
2022-03-30 17:45:29 +0200

    set incident-labels per inventory entry (#52)

--- cc/cra-custom-script.sh
+++ cc/cra-custom-script.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+echo
 echo "Doing cra setup"
 ARTIFACTORY_API_KEY=$(cat /config/otc_ARTIFACTORY_API_KEY)
 export ARTIFACTORY_API_KEY
@@ -6,3 +7,12 @@ DOCKERBUILDFLAGS="--build-arg ARTIFACTORY_API_KEY"
 export DOCKERBUILDFLAGS
 set_env cra-docker-build-context "true"
 echo "cra setup done"
+
+# workaround the fact that one cannot specify incident-labels per inventory entry for CRA in CC
+# see https://ibm-cloudplatform.slack.com/archives/CFQHG5PP1/p1648649959554529
+echo
+currentDir=$(pwd)
+repo=$(basename $currentDir)
+inventoryEntry=$(load_repo $repo inventory-entry)
+echo "set_env incident-labels \"squad:umbrella,$inventoryEntry\""
+set_env incident-labels "squad:umbrella,$inventoryEntry"
