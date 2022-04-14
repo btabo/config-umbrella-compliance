c50c7771e0d28c43c110e6e5b0072a01b25ba8e2
Navneet Kaushal <Navneet.Kaushal@ibm.com>
2022-04-14 15:43:24 -0400

    Add more environment variables what we have on dev

--- ci/otc-slack-broker/deploy_config.sh
+++ ci/otc-slack-broker/deploy_config.sh
@@ -15,4 +15,10 @@ export ENV_SENTINEL_TOOLCHAIN_CRN=crn:v1:staging:public:toolchain:us-south:a/79d
 export ENV_DEFAULT_WDEK=eyJjaXBoZXJ0ZXh0IjoiS2h0THNlTVVmZjZtcFZnUTBjNW1iTCs3a2NPUCtiMFJUVUhHZXhONWFSTmNEdksyNnVqVVA0aTlLQnM9IiwiaXYiOiJOUzZMWmxPVmxxWmNCZi9WIiwidmVyc2lvbiI6IjQuMC4wIiwiaGFuZGxlIjoiNTdlOGJiMmItZmE3Ni00OGY1LWEzMjMtMzU3NTBmZWYxN2RjIn0=
 export ENV_ENABLE_KMS_EVENTS=true
 export ENV_IAM_URL=https://iam.test.cloud.ibm.com
+export ENV_ENABLE_NEW_RELIC=true
+export ENV_ENABLE_SECRET_PICKER=true
+export ENV_ENV_ID=ibm:ys1:us-south
+export ENV_KMS_EVENT_WEBHOOK=https://otc-slack-broker.us-south.devops.dev.cloud.ibm.com/slack-broker/api/v1/events
+export ENV_NEW_RELIC_APP_NAME=otc-slack-broker-ys1
+export ENV_SECRETS_API_URL=https://otc-ui.us-south.devops.dev.cloud.ibm.com/devops/api/v1/secrets
 # none
